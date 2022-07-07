a56a39e5f9e890d22d663d8a9b33ded8ed6b4449
rihl4202 <88212189+rihl4202@users.noreply.github.com>
2022-07-07 09:48:05 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Camera from "../screens/camera"
+
+export default function App() {
+  return (
+    <Camera/>
+  );
+}
